7082327904bb9a7f9ddef41f6efab47d8371fec1
aarti <aarti.gupta745@gmail.com>
2025-05-14 16:01:54 +0530

    committed

---
+++ src/basic/DateEx.java
@@ -0,0 +1,15 @@
+package basic;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateEx 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(LocalDate.now()+"_"+LocalTime.now());
+
+	}
+
+}
